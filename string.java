2fed4d598ff2796f9656235dbe963aa9c9ba2024
kokilaeie <srikokila27@gmail.com>
2016-08-25 11:01:00 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class conversion
+{
+public static void main(String []args)
+{
+Scanner s=new Scanner(System.in);
+String a="1658";
+int b=Integer.parseInt(a);
+System.out.println("num: "+b);
+}
+}
