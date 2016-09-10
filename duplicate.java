444faadfd30a185b6134304537e9601352b0a3a6
kokilaeie <srikokila27@gmail.com>
2016-09-10 16:21:45 +0530

    Create duplicate.java

---
+++ duplicate.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class duplicate
+{
+public static void main(String []args)
+Scanner s=new Scanner(System.in);
+String a=s.next();
+Set se=new HashSet();
+se.add(a);
+System.out.println(se);
+}
+}
